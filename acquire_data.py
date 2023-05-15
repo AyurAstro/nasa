ac34079ef93d96b47df76a2ac368ba04fd6a9795
Renay Oshop <renayo@users.noreply.github.com>
2023-05-14 19:33:27 -0600

    Create acquire_data.py

---
+++ acquire_data.py
@@ -0,0 +1,24 @@
+from gnews import GNews
+import os
+from newspaper import Article
+import csv
+
+topics=['accident']
+topics=['accident','assassination','birth','coup','eruption','explosion','fire','game','hurricane','miracle','shooting','suicide','wedding']
+google_news = GNews(language='en', period='24h')
+
+for topic in topics:
+    
+    with open('today_'+topic+'.csv', mode='w') as csvfile:
+        topic_news = google_news.get_news(topic)
+        writer=csv.writer(csvfile, delimiter=',',quotechar='"')
+        for num in range(len(topic_news)):
+            url=topic_news[num]['url']
+            published_date=topic_news[num]['published date']
+            try:
+                article = google_news.get_full_article(url)
+                text=article.text
+                line=[topic,text,url,published_date]
+                writer.writerow(line)
+            except:
+                print('error')
