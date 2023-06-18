dab2b850a47c325ae98a6ce4d813c4a4c8337a10
Renay Oshop <renayo@users.noreply.github.com>
2023-06-18 08:32:46 -0600

    Update get GNews.py

--- code/get GNews.py
+++ code/get GNews.py
@@ -15,7 +15,9 @@ openai.api_key = config['openai_key']
 
 topics=['accident','political assassination','birth','political coup','volcano eruption','explosion','fire','game','religious miracle','shooting','suicide','wedding']
 
-google_news = GNews(language='en', exclude_websites=['hsvvoice.com','sunnewsonline.com','mckenziebanner.com','montrealgazette.com','legacy.com','publicpower.org','uniindia.com','environewsnigeria.com','ottawacitizen.com','bangkokpost.com','wng.com','eurasiareview.com','nationalpost.com','bleedingcool.com','keyc.com','tapinto.net','fashionista.com','romesentinel.com','northsidesun.com','tallahatchienews.ms','abc.net.au','rockpapershotgun.com','spokanecity.org','canyoncourier.com','lfpress.com','beincrypto.com','si.com','business2community.com','nst.com','healthnews.com','calgaryherald.com','kidspot.com','yahoo.com', 'cnn.com', 'imdb.com', 'forbes.com', 'newsweek.com', 'blackstarnews.com', 'barrons.com'])
+#topics=['shooting','suicide','wedding']
+
+google_news = GNews(language='en', exclude_websites=['etnow.com','charlotteobserver.com','says.com','seattletimes.com','news7noida.com','iheart.com','parade.com','triblive.com','wowt.com','mahoningmatters.com','rvtravel.com','newindianexpress.com','bizjournals.com','chronline.com','middle-east-online.com','ntnews.com.au','onenews.ph','wsj.com','dailytelegraph.com.au','thechronicle.com.au','bringmethenews.com','al.com','herald-citizen.com','thehockeynews.com','newsobserver.com','torontosun.com','news9.com','creators.com','thestreet.com','popkavoice.com','safety4sea.com','theolivepress.es','wafa.ps','news.com.au','windsorstar.com','newsdaytonabeach.com','thejournal-news.net','hsvvoice.com','sunnewsonline.com','mckenziebanner.com','montrealgazette.com','legacy.com','publicpower.org','uniindia.com','environewsnigeria.com','ottawacitizen.com','bangkokpost.com','wng.com','eurasiareview.com','nationalpost.com','bleedingcool.com','keyc.com','tapinto.net','fashionista.com','romesentinel.com','northsidesun.com','tallahatchienews.ms','abc.net.au','rockpapershotgun.com','spokanecity.org','canyoncourier.com','lfpress.com','beincrypto.com','si.com','business2community.com','nst.com','healthnews.com','calgaryherald.com','kidspot.com','yahoo.com', 'cnn.com', 'imdb.com', 'forbes.com', 'newsweek.com', 'blackstarnews.com', 'barrons.com'])
 
 now=datetime.datetime.now(gettz('US/Mountain'))
 print(now)
@@ -28,27 +30,34 @@ google_news.end_date = now
 
 for topic in topics:
     print(topic)
-    with open('today_'+topic+'.csv', mode='w',encoding="utf8") as csvfile:
+    with open('today_'+topic+'.csv', mode='w', encoding="utf8") as csvfile:
         topic_news = google_news.get_news(topic)
         topic_news1 = []
         for value in topic_news:
             if value not in topic_news1:
                 topic_news1.append(value)
         writer=csv.writer(csvfile, delimiter=',',quotechar='"')
-        for num in range(len(topic_news1)):    
+        print(len(topic_news1))
+        for num in range(len(topic_news1)):
+            line=[]
+            url=''
+            text=''
+            published_date=''
+            response={}   
             url=topic_news1[num]['url']
             published_date=topic_news1[num]['published date']
             try:
                 article = google_news.get_full_article(url)
-                text=article.text
-                response = openai.Completion.create(
-                  model="text-curie-001",
-                  prompt="From the following article, tell me on what day, at what time, and in what town the "+topic+" occurred.\n\n"+text[0:4000]+"...",
-                  temperature=0.3,
-                  max_tokens=80,
-                  top_p=1
-                )
-                line=[topic,text,url,published_date,response["choices"][0]["text"]]
-                writer.writerow(line)
+                text = article.text
+                if text.find("embargo") == -1:
+                    response = openai.Completion.create(
+                      model="text-curie-001",
+                      prompt="From the following article, tell me on what day, at what time, and in what town the "+topic+" occurred.\n\n"+text[0:4000]+"...",
+                      temperature=0.4,
+                      max_tokens=80,
+                      top_p=1
+                    )
+                    line=[topic,text,url,published_date,response["choices"][0]["text"]]
+                    writer.writerow(line)
             except:
-                print('error')      
+                print('error')
